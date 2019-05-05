2baa7aa941193306db772a32ef86e12f618d15d3
JINGYIDING <dingjy94@gmail.com>
2019-05-04 23:04:20 -0700

    finish 11

---
+++ 11 - Custom Video Player/README.md
@@ -0,0 +1,10 @@
+# Custom Video Player
+
+## Requirment
+Play, pause, control process. The range control volume, the second control playbackRate.
+
+## Relevent Resources
+- [playbackRate](https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/playbackRate)
+- [play event](https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/play_event)
+- [video events](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/video)
+
---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,52 @@
+const video = document.querySelector('.viewer');
+const playBtn = document.querySelector('.toggle');
+const player = document.querySelector('.player');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+const togglePlay = (e) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+
+const updateBtn = (e) => {
+  let icon = video.paused ? '►' : '❚ ❚';
+  playBtn.textContent = icon;
+};
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateBtn);
+video.addEventListener('pause', updateBtn);
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', handleProgress);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
